5b3ab99b61fe5cf8ab48b2da4effd1fc6671ea92
Aishwarya Palekar <apalekar@vmware.com>
2022-12-14 14:36:51 -0500

    Hello World 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 #import streamlit as st
-#st.write('Hello, world!')
 
 import streamlit as st
+st.write('Hello, world!')
 st.header('st.button')
 
 if st.button('Say hello'):
     st.write('Why hello there')
 else:
     st.write('Goodbye')
-    
