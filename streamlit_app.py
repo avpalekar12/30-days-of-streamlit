69df36d7fe630aa36aea7156455f1faa47942ddd
Aishwarya Palekar <apalekar@vmware.com>
2022-12-14 14:29:35 -0500

    Day 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
+#import streamlit as st
+#st.write('Hello, world!')
+
 import streamlit as st
-st.write('Hello, world!')
+st.header('st.button')
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
+    
