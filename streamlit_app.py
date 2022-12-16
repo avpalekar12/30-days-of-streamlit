fa441a568ad34ecdbe32dbb43842ce1e3824eef6
Aishwarya Palekar <apalekar@vmware.com>
2022-12-16 10:03:32 -0500

    Adds basic netflix visualization.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,21 @@
-#import streamlit as st
-
 import streamlit as st
-st.write('Hello, friend')
-st.header('st.button')
+import pandas as pd
 
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
+st.header('Netflix Data Visualizer by Country')
+
+netflix = pd.read_csv("https://raw.githubusercontent.com/practiceprobs/datasets/main/netflix-titles/netflix-titles.csv")
+
+filter_options = list()
+for column_name in netflix.columns:
+    if column_name not in ['country', 'title']:
+        filter_options.append(column_name)
+
+
+def visualize_data(y_axis):
+    chart_data = pd.DataFrame(netflix)
+    st.bar_chart(chart_data, x='country', y=y_axis)
+
+
+option = st.selectbox('Filter', filter_options)
+
+st.button('Generate Visualization', on_click=visualize_data(option))
